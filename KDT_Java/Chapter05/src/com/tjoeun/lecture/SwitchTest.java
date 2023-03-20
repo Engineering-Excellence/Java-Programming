50645e46f8f6aae1cd0d8e4346dd33f8b8cdc21e
Kyle <prophet@hanyang.ac.kr>
2023-03-20 23:57:16 +0900

    Perfect Number

--- KDT_Java/Chapter05/src/com/tjoeun/lecture/SwitchTest.java
+++ KDT_Java/Chapter05/src/com/tjoeun/lecture/SwitchTest.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class SwitchTest {
 
-	public static void main(String[] args) {
-		
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("3과목 점수를 입력하세요: ");
-		int java = scanner.nextInt();
-		int jsp = scanner.nextInt();
-		int spring = scanner.nextInt();
-		
-		int total = java + jsp + spring;
-		double average = (double) total / 3;
-		System.out.println("총점: " + total + "점, 평균: " + average);
-		
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("3과목 점수를 입력하세요: ");
+        int java = scanner.nextInt();
+        int jsp = scanner.nextInt();
+        int spring = scanner.nextInt();
+
+        int total = java + jsp + spring;
+        double average = (double) total / 3;
+        System.out.println("총점: " + total + "점, 평균: " + average);
+
 //		key에는 정수를 기억하는 변수 또는 연산 결과가 정수인 수식을 사용한다.
 //		JDK 1.7 부터는 key에 문자열도 사용할 수 있다.
 //		switch (key) {
@@ -29,29 +29,34 @@ public class SwitchTest {
 //				...;
 //				break;
 //		}
-		
-		switch ((int) average / 10) {
-			case 10:
+
+        switch ((int) average / 10) {
+            case 10:
 //				System.out.println("참잘했어요");
 //				System.out.println("참잘했어요\nA");
-				System.out.println("참잘했어요");
+                System.out.println("참잘했어요");
 //				break는 제어문(if는 제외)의 {} 블록을 탈출한다.
 //				break;
-			case 9:
-				System.out.println("A"); break;
-			case 8:
-				System.out.println("B"); break;
-			case 7:
-				System.out.println("C"); break;
-			case 6:
-				System.out.println("D"); break;
+            case 9:
+                System.out.println("A");
+                break;
+            case 8:
+                System.out.println("B");
+                break;
+            case 7:
+                System.out.println("C");
+                break;
+            case 6:
+                System.out.println("D");
+                break;
 //			case 5: case 4: case 3: case 2: case 1: case 0:
-			default:
-				System.out.println("F"); break;
-		}
-		
-	}
-	
+            default:
+                System.out.println("F");
+                break;
+        }
+
+    }
+
 }
 
 
