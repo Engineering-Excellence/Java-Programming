3ffcff41bb9d3b375e02e0d106a75b9d8941aba9
Kyle <prophet@hanyang.ac.kr>
2023-03-28 10:32:11 +0900

    First Java

---
+++ KOSA/Day01/src/kr/or/sw/lecture/FirstJava.java
@@ -0,0 +1,11 @@
+package kr.or.sw.lecture;
+
+class FirstJava {
+
+    public static void main(String[] args) {    // 진입점(시작점)
+
+        System.out.println("DaeBo");
+        System.out.print("Kyle");
+        System.out.printf("%n%d %c %s%n", 100, 'A', "kbs mbc");
+    }    // main end
+}    // class end
