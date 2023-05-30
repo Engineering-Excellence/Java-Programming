78fba21acf3c754069f43dd1c996c960d578b748
Kyle <prophet@hanyang.ac.kr>
2023-05-30 17:21:23 +0900

    HTML - CSS, JS

---
+++ KOSA/Day045_CSS_JS/src/main/webapp/js/main.js
@@ -0,0 +1,11 @@
+'use strict'
+
+window.onload = () => {
+
+}
+
+function hello() {
+    document.write('Kyle')
+    document.write('<h1><font color="blue">Daebo</font></h1>')
+    alert('script의 hello() 함수 실행')
+}
