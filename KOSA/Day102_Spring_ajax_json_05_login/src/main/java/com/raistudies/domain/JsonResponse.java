7e7e6484dc9ccf908e85c4802543f612dad8cefb
Kyle <prophet@hanyang.ac.kr>
2023-08-17 08:47:53 +0900

    Spring - Ajax Login

---
+++ KOSA/Day102_Spring_ajax_json_05_login/src/main/java/com/raistudies/domain/JsonResponse.java
@@ -0,0 +1,12 @@
+package com.raistudies.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JsonResponse {
+
+    private String status = null;
+    private Object result = null;
+}
---
+++ KOSA/Day102_Spring_ajax_json_05_login/src/main/java/com/raistudies/domain/User.java
@@ -0,0 +1,12 @@
+package com.raistudies.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+
+    private String name = null;
+    private String education = null;
+}
